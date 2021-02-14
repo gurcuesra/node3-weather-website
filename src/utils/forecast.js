39ebd74a9c539144236259ff9a8d65d3f52c2144
gurcuesra <gurcuesra@gmail.com>
2021-02-13 23:39:51 -0800

    Add new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,14 @@
 const request = require('postman-request');
 
+/**
+ * Goal: Add new data to forecast
+ * 
+ * 1. Update the forecast string to include new data
+ * 2. Commit your changes
+ * 3. Push your changes to Github and deploy to Heroku
+ * 4. Test your work in the live application!
+ */
+
 const forecast = (lat, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=285b270c8bca9d1f227fa3ee485f4fe7&query=${lat},${long}&units=f`;
 
@@ -9,7 +18,7 @@ const forecast = (lat, long, callback) => {
     } else if(body.error) {
       callback('Unable to find the location', undefined);
     } else {
-      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.feelslike}%.`)
     }
   })
 }
